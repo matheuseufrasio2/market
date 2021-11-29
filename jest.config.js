e21f9915e93350970a20a8606702a6060945fa95
Matheus Eufrasio <matheuseufrasio2@gmail.com>
2021-11-29 19:56:01 -0300

    Separando responsabilidade de testar pra outra branch de feature

--- jest.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  testEnvironment: "jsdom",
-  testPathIgnorePatterns: ["/node_modules/", "/.next/"],
-  setupFilesAfterEnv: ["<rootDir>/.jest/setup.ts"],
-  moduleDirectories: ["node_modules", "src"],
-};
